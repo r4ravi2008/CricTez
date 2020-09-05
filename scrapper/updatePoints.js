30c64dcc9a1760e25bd03f56fbaa2a6fd510a292
skukreja <skukreja434@gmail.com>
2020-09-05 17:37:40 +0530

    Update:Scrapper-Updated-For-First-Player

--- scrapper/updatePoints.js
+++ scrapper/updatePoints.js
@@ -8,11 +8,27 @@ const scrapePlayerPoints = async (url) => {
   const page = await browser.newPage();
   await page.goto(url);
   const data = await page.evaluate(() => {
+    let first = document
+      .querySelector(".top-players__top-player")
+      .innerText.replace(/(\r\n|\n|\r)/gm, "\t")
+      .split("\t");
+    const first_playerId = document.querySelectorAll(".js-headshot")[0]
+      .attributes["data-player-id"].value;
+    const first_rank = parseInt(first[0]);
+    const first_name = first[1] + "" + first[2];
+    const first_points = (first[3]);
     let table = document
       .getElementsByTagName("tbody")[0]
       .innerText.replace(/(\r\n|\n|\r)/gm, "\t")
       .split("\t");
-    let a = [];
+    let a = [
+      {
+        rank: first_rank,
+        name: first_name,
+        points: first_points,
+        playerId: first_playerId,
+      },
+    ];
     for (var i = 23; i < table.length; i += 11) {
       const rank = parseInt(table[i]);
       const name = table[i + 1];
@@ -29,6 +45,7 @@ const scrapePlayerPoints = async (url) => {
     }
     return a;
   });
+  console.log(data);
   data.forEach(async (item) => await updateData(item));
   await browser.close();
   return data;
@@ -45,6 +62,7 @@ const updateData = async (data) => {
   } catch (err) {
     //All players are not present in DB which usually causes 404 error
   }
+  console.log(totalPlayersUpdated);
 };
 
 scrapePlayerPoints("https://www.iplt20.com/stats/2019/player-points");
