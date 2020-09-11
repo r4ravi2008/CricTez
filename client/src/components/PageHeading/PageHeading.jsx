32b7f461986935520a96f51a4a043746ee007800
skukreja <skukreja434@gmail.com>
2020-09-12 02:11:02 +0530

    Add:Animation-Page-Heading

--- client/src/components/PageHeading/PageHeading.jsx
+++ client/src/components/PageHeading/PageHeading.jsx
@@ -3,15 +3,41 @@ import "./PageHeading.css";
 import { motion } from "framer-motion";
 
 function PageHeading({ text }) {
+  const variantsUp = {
+    initial: { opacity: 0, y: "-100%" },
+    final: { opacity: 1, y: 0, transition: { duration: 0.5, type: "tween" } },
+  };
+  const variantsDown = {
+    initial: { opacity: 0, y: "100%" },
+    final: { opacity: 1, y: 0, transition: { duration: 0.5, type: "tween" } },
+  };
   return (
-    <div className="page-heading-container">
+    <motion.div className="page-heading-container">
       <br />
-      <h4 className="page-heading">{text.split(" ")[0]}</h4>
-      <h1 className="page-subheading">{text.split(" ")[1]}</h1>
-      <br/>
+      <div style={{ overflow: "hidden", height: "3.2rem" }}>
+        <motion.h4
+          className="page-heading"
+          initial="initial"
+          animate="final"
+          variants={variantsDown}
+        >
+          {text.split(" ")[0]}
+        </motion.h4>
+      </div>
+      <div style={{ overflow: "hidden", height: "2rem" }}>
+        <motion.h1
+          className="page-subheading"
+          initial="initial"
+          animate="final"
+          variants={variantsUp}
+        >
+          {text.split(" ")[1]}
+        </motion.h1>
+      </div>
+      <br />
       <hr />
-      <br/>
-    </div>
+      <br />
+    </motion.div>
   );
 }
 
